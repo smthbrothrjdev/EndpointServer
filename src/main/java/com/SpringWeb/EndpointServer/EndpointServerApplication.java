916b72eb3d7132395c6975a099cdd74ae6e2c05f
Action Jackson <smthbrothrjdev@gmail.com>
2022-08-09 21:33:46 -0500

    intitial

---
+++ src/main/java/com/SpringWeb/EndpointServer/EndpointServerApplication.java
@@ -0,0 +1,13 @@
+package com.SpringWeb.EndpointServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringWeb/EndpointServer/EndpointServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringWeb.EndpointServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
